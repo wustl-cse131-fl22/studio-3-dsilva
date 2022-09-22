98a79210a75de51b9cbf4dec09fdd8c38a8e6405
dsilv <dsilv@Osiris.dhcp.wustl.edu>
2022-09-22 12:48:07 -0500

    Studio 3

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,29 @@
+package studio3;
+
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		try (// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in)) 
+			{
+			System.out.print("n amount: ");
+			int n = in.nextInt();
+			int[] intArray = new int[n];
+			for (int i = 0; i == n; i++)
+				
+				
+			intArray[0] = n;
+			for (int i = 2; i < n; i++)
+			{
+				System.out.print(intArray);
+			}
+			
+			
+			
+}
+		
+}
+}
